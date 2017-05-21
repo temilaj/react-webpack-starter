789848a49dfab8a28fbd0f6fbd0a77322e959bb2
Temi Lajumoke <temi@temilajumoke.com>
2017-05-21 12:10:27 +0100

    update entry Js file

--- src/js/app.js
+++ src/js/app.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+const formatName = user => `${user.firstName} ${user.lastName}`;
+
+const user = {
+  firstName: 'John',
+  lastName: 'Doe'
+};
+
+ReactDOM.render(
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>,
+  document.querySelector('#root')
+);
