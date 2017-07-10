af8bd379bcea14b91a6a6c8c7b3b41327d278d0d
Temi Lajumoke <temi@temilajumoke.com>
2017-07-10 18:35:06 +0100

    add production  webpack config

---
+++ webpack.config.production.js
@@ -0,0 +1,86 @@
+const path = require('path');
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const port = process.env.PORT || 3000;
+const outputPath = path.join(__dirname, "dist")
+
+module.exports = {
+    entry: './src/App.jsx',
+    output: {
+        path: __dirname,
+        filename: 'dist/bundle.js'
+    },
+    resolve: {
+        modules: ['node_modules', './src'],
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader!sass-loader'
+                }),
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    use: 'css-loader'
+                }),
+            },
+            {
+                test: /\.(js|jsx)$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/, 
+                query: {
+                    presets:['es2015','react']
+                }
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin("bundle.css"),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                screw_ie8: true,
+                warnings: false
+            },
+            mangle: {
+                screw_ie8: true
+            },
+            output: {
+                comments: false,
+                screw_ie8: true
+            },
+            minimize: true,
+            sourceMap: true
+        }),
+        new HtmlWebpackPlugin({
+            inject: true,
+            template: path.join(__dirname, '/index.html'),
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                removeRedundantAttributes: true,
+                useShortDoctype: true,
+                removeEmptyAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                keepClosingSlash: true,
+                minifyJS: true,
+                minifyCSS: true,
+                minifyURLs: true
+            }
+        }),
+        new webpack.EnvironmentPlugin({ NODE_ENV: 'production' }),
+    ],
+    devtool: "cheap-source-map",
+    devServer: {
+        port,
+        hot: true,
+        compress: true
+
+    }
+}
